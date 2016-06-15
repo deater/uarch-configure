b72a73dcbb6e05d24d2f9d987804126b37fa7e07
Vince Weaver <vincent.weaver@maine.edu>
2016-06-15 15:36:28 -0400

    apm-read: trying to make sense of amd15h_power driver

--- amd-apm-read/apm-read.c
+++ amd-apm-read/apm-read.c
@@ -10,6 +10,9 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
 
 #include <cpuid.h>
 
@@ -18,6 +21,11 @@ int main(int argc, char **argv) {
 	unsigned int eax,ebx,ecx,edx=0;
 	unsigned int stepping,model,family;
 	char vendor_string[13];
+	unsigned int running_avg_range,tdp_running_avg,running_avg_capture;
+	int tdp_limit,tdp_limit3,tdp_to_watts;
+	unsigned int processor_tdp,base_tdp,tdp;
+	int result;
+	int fd;
 
 	/* Get CPUID leaf 0 which has name and level */
 	__get_cpuid (0x0,&eax,&ebx,&ecx,&edx);
@@ -66,22 +74,115 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
+	/* APM is enabled if all of the following conditions are true: */
+	/* * MSRC001_0015[CpbDis] = 0 for all cores.
+	   * D18F4x15C[ApmMasterEn] = 1.
+	   * D18F4x15C[BoostSrc] = 01b.
+	   * D18F4x15C[NumBoostStates] != 0.
+	*/
+
+
 	/* the drivers/hwmon/fam15h_power.c tweaks the value in */
 	/* 00:18.5 Host bridge: Advanced Micro Devices, Inc. [AMD] Family 15h Processor Function 5 */
-	/* D18F5xE0 */
+	/* D18F5xE0 Processor TDP Running Average */
 	/* sudo setpci -s 18.5 0xe0.l */
 	/* only low 4 bits matter?  Seem to be 0x9 on 4.7/piledriver */
 	/* which should be right */
 
 
+	/* FIXME: we need to do this for each processor package */
 
-	/* APM is enabled if all of the following conditions are true: */
-	/* * MSRC001_0015[CpbDis] = 0 for all cores.
-	   * D18F4x15C[ApmMasterEn] = 1.
-	   *  D18F4x15C[BoostSrc] = 01b.
-	   * D18F4x15C[NumBoostStates] != 0.
-	*/
+	fd=open("/sys/bus/pci/devices/0000:00:18.5/config",O_RDONLY);
+	if (fd<0) {
+		printf("Couldn't open PCI: %s\n",strerror(errno));
+		return 0;
+	}
 
+	result=pread(fd,&tdp_running_avg,4,0xe0);
+	if (result<4) {
+		printf("Couldn't read PCI: %s\n",strerror(errno));
+		return 0;
+	}
+
+	running_avg_range=tdp_running_avg&0xf;
+
+	printf("Running avg range: %x\n",running_avg_range);
+	if (running_avg_range!=9) {
+		printf("Later documentation suggests setting this to 0x9\n");
+	}
+	close(fd);
+
+	/* Extended on excavator */
+	if ((family==0x15) && (model>=0x60)) {
+		running_avg_capture = tdp_running_avg >> 4;
+		/* sign extend starting bit 27? */
+	}
+	else {
+		running_avg_capture = (tdp_running_avg >> 4)&0x3fffff;
+		/* sign extend starting bit 21? */
+	}
+
+
+	/* D18F5xE8 TDP Limit 3 */
+	/* 28:16 ApmTdpLimit */
+	/* 9:0 Tdp2Watt fixed point 0.10 conversion factor */
+
+	fd=open("/sys/bus/pci/devices/0000:00:18.5/config",O_RDONLY);
+	if (fd<0) {
+		printf("Couldn't open PCI: %s\n",strerror(errno));
+		return 0;
+	}
+
+	result=pread(fd,&tdp_limit3,4,0xe8);
+	if (result<4) {
+		printf("Couldn't read PCI: %s\n",strerror(errno));
+		return 0;
+	}
+
+	if ((family==0x15) && (model>=0x60)) {
+		tdp_limit = tdp_limit3 >> 16;
+	}
+	else {
+		tdp_limit = (tdp_limit3 >> 16) & 0x1fff;
+	}
+	close(fd);
+
+	printf("TDP Limit=%d\n",tdp_limit);
+
+	/* This is what the fam15h_power.c driver does */
+	/* A lot of this doesn't seem to be documented?*/
+
+	/* read 18f4x1b8 REG_PROCESSOR_TDP -- undocumented? */
+	fd=open("/sys/bus/pci/devices/0000:00:18.4/config",O_RDONLY);
+	if (fd<0) {
+		printf("Couldn't open PCI: %s\n",strerror(errno));
+		return 0;
+	}
+
+	result=pread(fd,&processor_tdp,4,0x1b8);
+	if (result<4) {
+		printf("Couldn't read PCI: %s\n",strerror(errno));
+		return 0;
+	}
+
+	base_tdp=processor_tdp>>16;
+	tdp=processor_tdp&0xffff;
+
+	printf("ProcessorTDP: raw=%x, base=%d\n",
+		processor_tdp,base_tdp);
+
+	/* bits 10:15 are undocumented.  Lower part of fixed point? */
+	tdp_to_watts = ((tdp_limit3 & 0x3ff) << 6) | ((tdp_limit3 >> 10) & 0x3f);
+	tdp *= tdp_to_watts;
+
+	/* Must be < 256 */
+	if ((tdp >> 16) >= 256) {
+		printf("Error, TDP too high\n");
+		return 0;
+	}
+	printf("TDP=%d microwatts\n",tdp);
+	tdp=(tdp*15625)>>10;
+	printf("TDP=%d microwatts\n",tdp);
 
 	/* D18F5xE0 Processor TDP Running Average */
 	/* Time interval = 2^(RunAvgRange + 1) * FreeRunSampleTimer rate. */
@@ -89,23 +190,27 @@ int main(int argc, char **argv) {
 
 	/* D18F4x110 Sample and Residency Timers */
 
-	/* D18F5xE8 TDP Limit 3 */
-	/* 28:16 ApmTdpLimit */
-	/* 9:0 Tdp2Watt fixed point 0.10 conversion factor */
 
 	/* On our machine 029e007e 00 0111 1110 */
 
 
-/* D18F3 */
-#define REG_NORTHBRIDGE_CAP             0xe8
+	/****************************************************/
+	/* ApmPwrReporting: APM accumulated power reporting */
+	/****************************************************/
+	/* This is what the "perf" tool needs to provide rapl-like results */
+	/* Need Excavator (fam15h model60h) or Jaguar/blah (fam16h model 30h) */
+	/* Support is noted in cpuid 80000007:edx bit 12    */
+	/* See BKDG for AMD Family 15h Models 60h-6Fh Processors */
+	/*     BKDG for AMD Family 16h Models 30h-3Fh Processors */
 
-/* D18F4 */
-#define REG_PROCESSOR_TDP               0x1b8
+	/* MSRC001_007A Compute Unit Power Accumulator */
+	/* MSRC001_007B Max Compute Unit Power Accumulator */
 
-/* D18F5 */
-#define REG_TDP_RUNNING_AVERAGE         0xe0
-#define REG_TDP_LIMIT3                  0xe8
-
+	if (!(edx&(1<<12))) {
+		printf("Bit 12 (ApmPwrReporting) not set in cpuid 80000007:edx, no extra support\n\n");
+		return 0;
+	}
+	printf("APM Accumulated Power Supported!\n");
 
 	return 0;
 
