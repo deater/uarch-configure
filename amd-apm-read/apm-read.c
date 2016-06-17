76264cfbdd2a0e3b9c678ab738c675521d6be7d9
Vince Weaver <vincent.weaver@maine.edu>
2016-06-17 12:27:16 -0400

    apm-read: some more changes trying to figure out weird fam16h bugs

--- amd-apm-read/apm-read.c
+++ amd-apm-read/apm-read.c
@@ -7,59 +7,228 @@
 
 /* note http://lm-sensors.lm-sensors.narkive.com/5MI2vOwZ/wrong-output-with-fam15h-power */
 
+#include <stdint.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
+#include <math.h>
 
 #include <cpuid.h>
 
-int main(int argc, char **argv) {
+
+
+static int open_msr(int core) {
+
+	char msr_filename[BUFSIZ];
+	int fd;
+
+	sprintf(msr_filename, "/dev/cpu/%d/msr", core);
+	fd = open(msr_filename, O_RDONLY);
+	if ( fd < 0 ) {
+		if ( errno == ENXIO ) {
+			fprintf(stderr, "rdmsr: No CPU %d\n", core);
+			return -1;
+		} else if ( errno == EIO ) {
+			fprintf(stderr, "rdmsr: CPU %d doesn't support MSRs\n",
+				core);
+			return -1;
+		} else {
+			perror("rdmsr:open");
+			fprintf(stderr,"Trying to open %s\n",msr_filename);
+			return -1;
+		}
+	}
+
+	return fd;
+}
+
+
+static uint64_t read_msr(int fd, unsigned int which) {
+
+	uint64_t data;
+
+	if ( pread(fd, &data, sizeof data, which) != sizeof data ) {
+		perror("rdmsr:pread");
+		return -1;
+	}
+
+	printf("MSR: read %"PRIx64"\n",data);
+
+	return data;
+}
+
+#if 0
+static unsigned long long rdtsc(void) {
+
+	unsigned a,d;
+
+	__asm__ volatile("rdtsc" : "=a" (a), "=d" (d));
+
+	return ((unsigned long long)a) | (((unsigned long long)d) << 32);
+}
+#endif
+
+
+
+/****************************************************/
+/* ApmPwrReporting: APM accumulated power reporting */
+/****************************************************/
+/* This is what the "perf" tool needs to provide rapl-like results */
+/* Need Excavator (fam15h model60h) or Jaguar/blah (fam16h model 30h) */
+/* Support is noted in cpuid 80000007:edx bit 12    */
+/* See BKDG for AMD Family 15h Models 60h-6Fh Processors */
+/*     BKDG for AMD Family 16h Models 30h-3Fh Processors */
+/* For algorithm, see 17.5 in a 2016 edition of the */
+/*     AMD64 Architecture Programmer's Manual, Volume 2 */
+int test_apm_pwr_reporting(void) {
 
 	unsigned int eax,ebx,ecx,edx=0;
-	unsigned int stepping,model,family;
-	char vendor_string[13];
+	int fd,tsc_size;
+	long long PwrCPUave,N,Tx,Ty,JMax,Jx,Jy,Jdelta,Tdelta;
+	double tsc_overflow;
+
+	printf("\n###############################\n");
+	printf("Testing APM Accumulerated Power\n");
+	printf("###############################\n\n");
+
+	/* Support is noted in cpuid 80000007:edx bit 12    */
+	__get_cpuid (0x80000007,&eax,&ebx,&ecx,&edx);
+	if (!(edx&(1<<12))) {
+		printf("Bit 12 (ApmPwrReporting) not set in cpuid 80000007:edx, no extra support\n\n");
+		return -1;
+	}
+	printf("APM Accumulated Power Supported!\n");
+
+
+	/* From 17.5.2 */
+	/* To obtain a stable average power value, */
+	/* Tm should be on the order of several milliseconds. */
+
+	/* Determine the value of the ratio of Tsample to the TSC period */
+	/* (CpuPwrSampleTimeRatio) by executing CPUID Fn8000_0007. */
+	/* Call this value N. */
+	N=ecx;
+	printf("N=%lld\n",N);
+
+
+	/* Check for perf_tsc */
+	/* Support is noted in cpuid 80000001:ecx bit 27    */
+	__get_cpuid (0x80000001,&eax,&ebx,&ecx,&edx);
+	if (!(ecx&(1<<27))) {
+		printf("Bit 27 (ApmPwrReporting) not set in cpuid 80000001:ecx, no extra support\n\n");
+		return -1;
+	}
+
+	/* Check size of perf_tsc */
+	/* Noted in cpuid 80000008:ecx bits 16-17    */
+	__get_cpuid (0x80000008,&eax,&ebx,&ecx,&edx);
+	switch( (ecx>>16)&3) {
+		case 0:	tsc_size=40; break;
+		case 1: tsc_size=48; break;
+		case 2: tsc_size=56; break;
+		case 3: tsc_size=64; break;
+	}
+
+	tsc_overflow=pow(2,tsc_size)/100000000.0;
+
+	printf("APM perf_tsc detected, size %d bits, overflow in %lfs!\n",
+		tsc_size,tsc_overflow);
+
+
+	/* Read the full range of the cumulative energy value from */
+	/* the register MaxCpuSwPwrAcc.				*/
+	/* Jmax = value returned from RDMSR MaxCpuSwPwrAcc.	*/
+	/* MSRC001_007B Max Compute Unit Power Accumulator */
+
+	fd=open_msr(0);
+	if (fd<0) {
+		return -1;
+	}
+
+	JMax=read_msr(fd,0xc001007bULL);
+	printf("JMax=%lld\n",JMax);
+
+	printf("\nMeasuring power while sleeping 5ms\n");
+
+	/*At time x, read CpuSwPwrAcc and the TSC */
+	/* J_x = value returned by RDMSR CpuSwPwrAcc	*/
+	/* T_x = value returned by RDTSC		*/
+	/* MSRC001_007A Compute Unit Power Accumulator */
+	/* Note: Linux driver uses PTSC rather than RDTSC */
+	/* MSRC001_0280 */
+	Jx=read_msr(fd,0xc001007aULL);
+//	Tx=rdtsc();
+	Tx=read_msr(fd,0xc0010280ULL);
+
+
+
+	/* sleep for 5ms */
+//	usleep(5000);
+
+#if 1
+	{ int i; double a=0.0;
+	for(i=0;i<20000000;i++) {
+		a+=drand48();
+		a-=drand48();
+		a=a*a;
+
+	}
+	printf("%lf\n",a);
+	}
+#endif
+
+	/* At time y, read CpuSwPwrAcc and the TSC again	*/
+	/* J_y = value returned by RDMSR CpuSwPwrAcc		*/
+	/* T_y = value returned by RDTSC			*/
+	Jy=read_msr(fd,0xc001007aULL);
+	Ty=read_msr(fd,0xc0010280ULL);
+	//Ty=rdtsc();
+
+	/* Calculate the average power consumption for the processor	*/
+	/* core over the measurement interval T_M = (Ty – Tx).		*/
+	/* If (J y < J x ), rollover has occurred; */
+	/*    set Jdelta = (J y + Jmax) – J x	*/
+	/* else Jdelta = J y – J x	*/
+	/* PwrCPUave = N * Jdelta / (T y - T x ) */
+
+	if (Jy<Jx) {
+		Jdelta=(Jy+JMax)-Jx;
+	}
+	else {
+		Jdelta=(Jy-Jx);
+	}
+
+	if (Ty<Tx) {
+		Tdelta=(Ty+(1ULL<<tsc_size))-Tx;
+	}
+	else {
+		Tdelta=(Ty-Tx);
+	}
+
+	PwrCPUave=N*Jdelta / (Tdelta);
+
+	printf("deltaJ=%lld deltaT=%lld (%llx - %llx)\n",Jdelta,Tdelta,
+		Ty,Tx);
+	printf("PwrCPUave=%lldmW = %fW\n",
+		PwrCPUave,(double)PwrCPUave/1000.0);
+
+	return 0;
+}
+
+
+int test_tdp_reporting(int is_excavator) {
+
+	unsigned int eax,ebx,ecx,edx=0;
 	unsigned int running_avg_range,tdp_running_avg,running_avg_capture;
 	int tdp_limit,tdp_limit3,tdp_to_watts;
 	unsigned int processor_tdp,base_tdp,tdp;
-	int result;
-	int fd;
-
-	/* Get CPUID leaf 0 which has name and level */
-	__get_cpuid (0x0,&eax,&ebx,&ecx,&edx);
-	memcpy(vendor_string,&ebx,4);
-	memcpy(vendor_string+4,&edx,4);
-	memcpy(vendor_string+8,&ecx,4);
-	vendor_string[12]=0;
-
-	__get_cpuid (0x1,&eax,&ebx,&ecx,&edx);
-	stepping=eax&0xf;
-	model=(eax>>4)&0xf;
-	family=(eax>>8)&0xf;
-
-	/* All newer procs?? */
-	family+=((eax>>20)&0xff);
-	model+=(((eax>>16)&0xf)<<4);
-
-	printf("Looking for AMD APM support...\n");
-
-	printf("\tFound family %d model %d stepping %d %s processor\n",
-		family,model,stepping,vendor_string);
-
-	if (strcmp("AuthenticAMD",vendor_string)) {
-		printf("Not an AMD processor!  Exiting.\n\n");
-
-		return 0;
-	}
-
-	if (family<0x15) {
-		printf("Family %x processor too old (need at least 0x15)\n\n",
-			family);
-
-		return 0;
-	}
+	int fd,result;
 
 	/* 2.5.9.1 */
 	/* Support for APM is specified by CPUID Fn8000_0007_EDX[CPB]. */
@@ -110,7 +279,7 @@ int main(int argc, char **argv) {
 	close(fd);
 
 	/* Extended on excavator */
-	if ((family==0x15) && (model>=0x60)) {
+	if (is_excavator) {
 		running_avg_capture = tdp_running_avg >> 4;
 		/* sign extend starting bit 27? */
 	}
@@ -136,7 +305,7 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
-	if ((family==0x15) && (model>=0x60)) {
+	if (is_excavator) {
 		tdp_limit = tdp_limit3 >> 16;
 	}
 	else {
@@ -190,24 +359,58 @@ int main(int argc, char **argv) {
 
 	/* On our machine 029e007e 00 0111 1110 */
 
+	(void)running_avg_capture;
 
-	/****************************************************/
-	/* ApmPwrReporting: APM accumulated power reporting */
-	/****************************************************/
-	/* This is what the "perf" tool needs to provide rapl-like results */
-	/* Need Excavator (fam15h model60h) or Jaguar/blah (fam16h model 30h) */
-	/* Support is noted in cpuid 80000007:edx bit 12    */
-	/* See BKDG for AMD Family 15h Models 60h-6Fh Processors */
-	/*     BKDG for AMD Family 16h Models 30h-3Fh Processors */
+	return 0;
+}
 
-	/* MSRC001_007A Compute Unit Power Accumulator */
-	/* MSRC001_007B Max Compute Unit Power Accumulator */
 
-	if (!(edx&(1<<12))) {
-		printf("Bit 12 (ApmPwrReporting) not set in cpuid 80000007:edx, no extra support\n\n");
+int main(int argc, char **argv) {
+
+	unsigned int eax,ebx,ecx,edx=0;
+	unsigned int stepping,model,family;
+	char vendor_string[13];
+	int is_excavator=0;
+
+	/* Get CPUID leaf 0 which has name and level */
+	__get_cpuid (0x0,&eax,&ebx,&ecx,&edx);
+	memcpy(vendor_string,&ebx,4);
+	memcpy(vendor_string+4,&edx,4);
+	memcpy(vendor_string+8,&ecx,4);
+	vendor_string[12]=0;
+
+	__get_cpuid (0x1,&eax,&ebx,&ecx,&edx);
+	stepping=eax&0xf;
+	model=(eax>>4)&0xf;
+	family=(eax>>8)&0xf;
+
+	/* All newer procs?? */
+	family+=((eax>>20)&0xff);
+	model+=(((eax>>16)&0xf)<<4);
+
+	printf("Looking for AMD APM support...\n");
+
+	printf("\tFound family %d model %d stepping %d %s processor\n",
+		family,model,stepping,vendor_string);
+
+	if (strcmp("AuthenticAMD",vendor_string)) {
+		printf("Not an AMD processor!  Exiting.\n\n");
+
 		return 0;
 	}
-	printf("APM Accumulated Power Supported!\n");
+
+	if (family<0x15) {
+		printf("Family %x processor too old (need at least 0x15)\n\n",
+			family);
+
+		return 0;
+	}
+
+	if ((family==0x15) && (model>=0x60)) is_excavator=1;
+
+	test_tdp_reporting(is_excavator);
+
+	test_apm_pwr_reporting();
 
 	return 0;
 
